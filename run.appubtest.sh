3c391d166aa06bd290a6c193ae3d437d4475dfb1
Nemo HunJae Lee <nemo.hj.lee@gmail.com>
2013-09-16 13:11:57 +0900

    under testing

--- run.appubtest.sh
+++ run.appubtest.sh
@@ -1,7 +1,5 @@
 export CLASSPATH=./lib/jeromq-0.3.0-SNAPSHOT.jar
-export CLASSPATH=$CLASSPATH:./lib/libthrift-0.9.1.jar
-export CLASSPATH=$CLASSPATH:./lib/log4j-1.2.16.jar
-export CLASSPATH=$CLASSPATH:./lib/slf4j-api-1.6.6.jar
+export CLASSPATH=$CLASSPATH:./lib/*
 export CLASSPATH=$CLASSPATH:./dist/eventhub.jar
 export CLASS=TestHttpPublisher
 java -cp $CLASSPATH $CLASS
